d5f8f6e6466cfb919d1a1f25c64f76e5bfc367c1
An Jaebeom <ajb8533296@gmail.com>
2022-12-16 19:59:08 +0900

    finish main.py

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+import time
+import requests
 from Recognizer import Recognizer
 
 class Main:
     def __init__(self):
         self.cam_ip = {"1": "192.168.15.89",
                        "2": "192.168.15.204"}
-        self.server_link = ""
+        self.server_link = "http://119.63.246.253:8000"
 
     def run(self):
-        video_class = Recognizer()
-        video_class.roomIp = self.cam_ip
+        while True:
+            video_class = Recognizer()
+            video_class.roomIp = self.cam_ip
 
+            result = video_class.run(False)
+            print(result)
 
-        result = video_class.run(False)
-        print(result)
+            for key, value in result.items():
+                requests.post(f"{self.server_link}/api/", json={"room": int(key), "count": int(value)})
+
+            time.sleep(10)
 
 main = Main()
 main.run()
